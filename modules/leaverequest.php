e1133c147a87d96c59bc57eb60071d7411b4aecd
Jhao <jsglaraga2000@gmail.com>
2026-07-09 07:16:20 +0800

    Leave Request Subtitle change

--- modules/leaverequest.php
+++ modules/leaverequest.php
@@ -490,7 +490,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['save'])) {
                         </button>
                         <div class="tk-form-hint" id="saveHint">
                             <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="4" y="10" width="16" height="10" rx="2"/><path d="M8 10V7a4 4 0 0 1 8 0v3"/></svg>
-                            <span id="saveHintText">Fill in the required leave details to enable submission.</span>
+                            <span id="saveHintText">Fill in the required leave details.</span>
                         </div>
                     </form>
                 </div>
